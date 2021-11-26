25e711af604eb5ae156248bb8edc61df8b40a408
Александр Ковшов <alexander@Aleksandrs-MacBook-Pro.local>
2021-11-26 23:00:59 +0300

    w

--- theLoopKey/src/app/map-timer-customer/map-timer-customer.component.ts
+++ theLoopKey/src/app/map-timer-customer/map-timer-customer.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-map-timer-customer',
   templateUrl: './map-timer-customer.component.html',
   styleUrls: ['./map-timer-customer.component.css']
 })
 export class MapTimerCustomerComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
 
 }
