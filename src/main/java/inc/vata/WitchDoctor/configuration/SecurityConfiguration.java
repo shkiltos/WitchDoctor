5039875f4053c8d65f7425306b9a066ec6d4e873
Vladimir Orlov <vladimir.orlov@akvelon.com>
2021-11-26 12:42:47 +0300

    Fixed broken dependencies

---
+++ src/main/java/inc/vata/WitchDoctor/configuration/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package inc.vata.WitchDoctor.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfiguration {
+
+//    @Bean
+//    public PasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder(this.passwordHashGenStrength);
+//    }
+}
