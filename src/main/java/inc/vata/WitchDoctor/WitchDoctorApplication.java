b027d9780dbb6d3ea951b427d41789eecccfaeb6
trofim.kaufmann <opendev-KaufmannTV@mcplat.ru>
2021-11-26 10:14:58 +0300

    Init commit

---
+++ src/main/java/inc/vata/WitchDoctor/WitchDoctorApplication.java
@@ -0,0 +1,13 @@
+package inc.vata.WitchDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitchDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/vata/WitchDoctor/WitchDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.vata.WitchDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitchDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
