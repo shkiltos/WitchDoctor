5303f37bda88fd47a266019c18a6b9bc3b6b6f4e
Vladimir Orlov <vladimir.orlov@akvelon.com>
2021-11-26 13:03:24 +0300

    Fixed broken dependencies

--- src/main/java/inc/vata/WitchDoctor/configuration/SecurityConfiguration.java
+++ src/main/java/inc/vata/WitchDoctor/configuration/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package inc.vata.WitchDoctor.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Value("${witchdoctor.security.bCryptRounds:#{12}}")
+    private Integer passwordHashGenStrength;
+
+    @Value("${witchdoctor.api.prefix:{/api}}")
+    private String apiPrefix;
 
 //    @Bean
 //    public PasswordEncoder bCryptPasswordEncoder() {
