83e1cc5ebf3863976f5fd68f4076b7eca0525543
Anton <>
2021-11-26 22:42:26 +0300

    Moved map to right place

--- theLoopKey/src/app/map-timer-customer/map-timer-customer.component.ts
+++ theLoopKey/src/app/map-timer-customer/map-timer-customer.component.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../node_modules/@types/google.maps/index.d.ts"/>
+
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-map-timer-customer',
@@ -9,15 +9,8 @@ export class MapTimerCustomerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.initMap();
-  }
-
-  private initMap(): void {
     
-
-    var map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
   }
+
+
 }
