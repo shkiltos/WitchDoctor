9e672898b56d989b2ce0fc12fedc0801f19512d4
trofim.kaufmann <opendev-KaufmannTV@mcplat.ru>
2021-11-27 05:09:19 +0300

    Fixed street selection

--- theLoopKey/src/app/initial-form/initial-form.component.ts
+++ theLoopKey/src/app/initial-form/initial-form.component.ts
@@ -47,14 +47,122 @@ export class InitialFormComponent implements OnInit {
     {viewValue: 'Тимакова улица'},
     {viewValue: 'Советской Армии улица'},
     {viewValue: 'Васильевская улица'},
+    {viewValue: 'Васильевская проезд'},
     {viewValue: 'Новосёлов улица'}
   ];
 
-  houses: any[] = [
-    {viewValue: '1'},
-    {viewValue: '2'},
-    {viewValue: '3'},
-  ];
+  houses: any[] = [];
+
+  public initHouses(event: any) {
+    if (event == '1-й Бульварный проезд') {
+      this.houses = [
+        {viewValue: '2'}
+      ];
+    }
+    if (event == '2-й Бульварный проезд') {
+      this.houses = [
+        {viewValue: '1'},
+        {viewValue: '2'},
+        {viewValue: '3'},
+        {viewValue: '4'},
+        {viewValue: '6'},
+        {viewValue: '8'}
+      ];
+    }
+    if (event == 'Большая улица') {
+      this.houses = [
+        {viewValue: '7'},
+        {viewValue: '7к1'},
+        {viewValue: '7к2'},
+        {viewValue: '15'},
+        {viewValue: '15к1'},
+        {viewValue: '17'},
+        {viewValue: '17к2'},
+        {viewValue: '19'},
+        {viewValue: '19к2'},
+        {viewValue: '90'},
+        {viewValue: '90к1'},
+        {viewValue: '92'},
+        {viewValue: '94'},
+        {viewValue: '94к1'},
+        {viewValue: '100'},
+        {viewValue: '102'},
+        {viewValue: '102к1'}
+      ];
+    }
+    if (event == 'Бульвар Оптимистов проезд') {
+      this.houses = [
+        {viewValue: '12'}
+      ];
+    }
+    if (event == 'Зелёная улица') {
+      this.houses = [
+        {viewValue: '17'},
+        {viewValue: '19'},
+        {viewValue: '21'},
+        {viewValue: '23'}
+      ];
+    }
+    if (event == 'Касимовское шоссе') {
+      this.houses = [
+        {viewValue: '5'},
+        {viewValue: '7'},
+        {viewValue: '7к1'},
+        {viewValue: '12'},
+        {viewValue: '14/9'},
+        {viewValue: '38к1'},
+        {viewValue: '40к1'},
+        {viewValue: '40к2'},
+        {viewValue: '42'},
+        {viewValue: '42к1'},
+        {viewValue: '44'},
+        {viewValue: '44к1'}
+      ];
+    }
+    if (event == 'Тимакова улица') {
+      this.houses = [
+        {viewValue: '2'},
+        {viewValue: '4'},
+        {viewValue: '6'},
+        {viewValue: '6к1'},
+        {viewValue: '6к2'},
+        {viewValue: '8'},
+        {viewValue: '8к1'},
+        {viewValue: '10'}
+      ];
+    }
+    if (event == 'Советской Армии улица') {
+      this.houses = [
+        {viewValue: '12'},
+        {viewValue: '12к1'},
+        {viewValue: '14'}
+      ]
+    }
+    if (event == 'Васильевская улица') {
+      this.houses = [
+        {viewValue: '49'},
+        {viewValue: '51'},
+        {viewValue: '51к1'},
+        {viewValue: '51к2'},
+        {viewValue: '51к3'},
+        {viewValue: '53'},
+        {viewValue: '53к1'},
+        {viewValue: '53к2'}
+      ]
+    }
+    if (event == 'Васильевская проезд') {
+      this.houses = [
+        {viewValue: '5'},
+        {viewValue: '7'},
+        {viewValue: '10'}
+      ]
+    }
+    if (event == 'Новосёлов улица') {
+      this.houses = [
+        {viewValue: '3'}
+      ]
+    }
+  }
 
   callData: any = {
   };
